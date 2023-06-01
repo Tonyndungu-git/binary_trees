555ea70386b935110d472364469a477dda430391
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-06-01 17:56:58 +0300

    counts no of nodes with one branch

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,12 +1,13 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - counts no of nodes with one branch
+ * binary_tree_nodes - counts no of nodes with one branch
  * @tree: binary tree
  * Return: size_t
  */
 
-size_t binary_tree_nodes(const binary_tree_t *tree) {
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
 	if (tree == NULL)
 		return (0);
 	if (tree->left != NULL || tree->right != NULL)
