7ed972f5d59917310b3df2f8a91973011de7b996
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-06-01 18:58:01 +0300

    checks balance btwn left and right branches

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+size_t binary_tree_height(const binary_tree_t *tree);
+
+/**
+ * binary_tree_balance - checks the balance of a tree.
+ * @tree: the binary tree.
+ * Return: int
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	return ((int)(left_height - right_height));
+}
+
+/**
+ * binary_tree_height - checks the height of a tree.
+ * @tree: the binary tree.
+ * Return: size-t
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
