7e74d216481ec66b5b314f36f1642944820df674
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-06-01 20:26:25 +0300

    checks if the the left and right brances are the same.

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,45 @@
+#include "binary_trees.h"
+
+size_t binary_tree_height(const binary_tree_t *tree);
+
+/**
+ * binary_tree_is_perfect - checks if the the left and right branches the same.
+ * @tree: the binary tree.
+ * Return: int
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	if (left_height == right_height)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			return (1);
+
+		return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+	}
+
+	return (0);
+}
+
+/**
+ * binary_tree_height - checks the height of a tree.
+ * @tree: the binary tree.
+ * Return: size-t
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
