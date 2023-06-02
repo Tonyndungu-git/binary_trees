fce561b9c1ca56d43b312810c6b020e6a72a1adf
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-06-02 20:13:44 +0300

    correction 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,16 +7,15 @@
  **/
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t depth = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	size_t depth = 0;
-	const binary_tree_t *current = tree;
-
-	while (current->parent != NULL)
+	while (tree->parent)
 	{
 		depth++;
-		current = current->parent;
+		tree = tree->parent;
 	}
 	return (depth);
 }
