e45252167097d4e2724b13bcaee4e6bc9a6e0100
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-06-02 22:23:32 +0300

    correction 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -20,7 +20,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_height - checks the height of a tree.
+ * binary_tree_height1 - checks the height of a tree.
  * @tree: the binary tree.
  * Return: size-t
  */
@@ -37,29 +37,4 @@ int binary_tree_height1(const binary_tree_t *tree)
 		return (h_left + 1);
 
 	return (h_right + 1);
-}
-
-int main(void)
-{
-    binary_tree_t *root;
-    int balance;
-
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(root, 128);
-    binary_tree_insert_left(root, 45);
-    binary_tree_insert_right(root->left, 50);
-    binary_tree_insert_left(root->left->left, 10);
-    binary_tree_insert_left(root->left->left->left, 8);
-    binary_tree_print(root);
-
-    balance = binary_tree_balance(root);
-    printf("Balance of %d: %+d\n", root->n, balance);
-    balance = binary_tree_balance(root->right);
-    printf("Balance of %d: %+d\n", root->right->n, balance);
-    balance = binary_tree_balance(root->left->left->right);
-    printf("Balance of %d: %+d\n", root->left->left->right->n, balance);
-    return (0);
 }
