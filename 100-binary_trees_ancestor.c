b5ef935b416d87b261414248dc58251abcbf4936
tony ndungu (AMN) <anthonymsalami@gmail.com>
2023-06-02 21:42:21 +0300

    find the ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -6,8 +6,8 @@
  * @second:node
  * Return: pointer
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tr
-				     ee_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+										const binary_tree_t *second)
 {
 	binary_tree_t *node;
 
